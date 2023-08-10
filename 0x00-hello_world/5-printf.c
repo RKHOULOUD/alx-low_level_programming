59f449033d5111acc94d5d9d0e57cd7fc5c66db5
khouloud <rajie.khouloud@gmail.com>
2023-08-10 15:01:17 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main = entry print
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer . but the outcom is a piece of art.\n");
+	return (0);
+}
